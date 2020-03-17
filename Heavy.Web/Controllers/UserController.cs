858252643e624e9d8b3d7668bbcea1eb63378ca8
Neil Wong <kk11235@qq.com>
2020-03-17 20:30:36 +0800

    添加UserController路由

---
+++ Heavy.Web/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Heavy.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Heavy.Web.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(UserCreateViewModel userCreateViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(userCreateViewModel);
+            }
+            var user = new IdentityUser
+            {
+                UserName = userCreateViewModel.UserName,
+                Email = userCreateViewModel.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, userCreateViewModel.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", await _userManager.Users.ToListAsync());
+            }
+
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(userCreateViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if(user!=null)
+            {
+                var result = await _userManager.DeleteAsync(user);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "删除用户发生错误");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "无此用户");
+            }
+            return View("Index", await _userManager.Users.ToListAsync());
+        }
+    }
+}
